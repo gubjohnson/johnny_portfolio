41eb60d1599370d27f720043ea56a977a3569e36
Johnny Chen <gubjohnson07@gmail.com>
2018-08-29 12:29:02 +1000

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,7 +13,7 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
-        menu.classList.add('close');
+        menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
@@ -22,7 +22,7 @@ function toggleMenu() {
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
-        menu.classList.remove('close');
+        menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
